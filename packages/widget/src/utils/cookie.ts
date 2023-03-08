594bfa91de37cf511f93142b21c7007e2067d335
Sonny T <mail@sonnyt.com>
2023-03-08 08:02:49 -0500

    using hostname instead of root domain

--- packages/widget/src/utils/cookie.ts
+++ packages/widget/src/utils/cookie.ts
@@ -1,22 +1,3 @@
-export function getDomain() {
-  let domain = null;
-
-  const hostname = window.location.hostname;
-  const parts = hostname.split('.');
-  const name = `__t${new Date().getTime()}`;
-
-  for (let i = 0; i < parts.length; i++) {
-    domain = parts.slice(-1 - i).join('.');
-
-    setCookie(name, null, domain);
-
-    if (getCookie(name)) {
-      removeCookie(name, domain);
-      return domain;
-    }
-  }
-}
-
 export function getCookie(name: string) {
   name = `${name}=`;
 
@@ -44,11 +25,11 @@ export function setCookie(
   domain?: string,
   expires = 'Fri, 31 Dec 9999 23:59:59 GMT'
 ) {
-  domain = domain ?? getDomain();
+  domain = domain ?? window.location.hostname;
   document.cookie = `${name}=${data};expires=${expires};path=/;domain=${domain};`;
 }
 
 export function removeCookie(name: string, domain?: string) {
-  domain = domain ?? getDomain();
+  domain = domain ?? window.location.hostname;
   setCookie(name, null, domain, 'Thu, 01 Jan 1970 00:00:00 UTC');
 }
