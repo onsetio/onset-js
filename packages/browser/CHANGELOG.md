2248eae5acd403ec1774297bb4fff593d34b9be0
Sonny T <mail@sonnyt.com>
2026-07-29 14:04:11 -0400

    fix: update package description and version; fix typing in fetch method

--- packages/browser/CHANGELOG.md
+++ packages/browser/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @onset/browser
 
+## 1.0.3
+
+### Patch Changes
+
+- fixed minor typing bug
+
 ## 1.0.2
 
 ### Patch Changes
--- packages/browser/src/workspace.ts
+++ packages/browser/src/workspace.ts
@@ -4,14 +4,14 @@ import type { Workspace as WorkspaceType } from "./types";
 export class Workspace {
   constructor(private page: string) {}
 
-  async fetch(): Promise<WorkspaceType[]> {
-    const { data: workspace, status } = await axios.get<WorkspaceType[]>(
-      `https://${this.page}/data.json`
+  async fetch(): Promise<WorkspaceType> {
+    const { data: workspace, status } = await axios.get<WorkspaceType>(
+      `https://${this.page}/data.json`,
     );
 
     if (status !== 200) {
       throw new Error(
-        `[ONSET] - Failed to fetch workspace data for page: ${this.page}`
+        `[ONSET] - Failed to fetch workspace data for page: ${this.page}`,
       );
     }
 
